02dcd61ab9fd9e91dc50bce800d58cf5e85c7bd6
EllisAC <ellisac@jncb.com>
2021-12-16 11:54:26 -0500

    Reading from a File

--- src/inputoutputFiles/inputOutputDemo.java
+++ src/inputoutputFiles/inputOutputDemo.java
@@ -6,11 +6,12 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class inputOutputDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File emloyeeFile = new File("C:\\Users\\ellisac\\Documents\\FullStackJava\\fullstackjava\\src\\inputoutputFiles\\EmployeeInfo.txt");
         inputOutputDemo demo = new inputOutputDemo();
         demo.writeToFile(emloyeeFile);
 
+        demo.readFile(emloyeeFile);
 
     }
 
@@ -30,6 +31,11 @@ public class inputOutputDemo {
 
     }
 
-
+    public void readFile(File fileToRead) throws FileNotFoundException {
+        Scanner input = new Scanner(fileToRead);
+        while(input.hasNext()){
+            System.out.println("Line read from the input file : " + input.nextLine());
+        }
+    }
 
 }
