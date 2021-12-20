7251d650fbcd11bf9ebe26656eeb0c90c10fa42e
EllisAC <ellisac@jncb.com>
2021-12-20 15:46:13 -0500

    Wrapper Classes

---
+++ src/WrapperClasses/Wrappers.java
@@ -0,0 +1,12 @@
+package WrapperClasses;
+
+public class Wrappers {
+    public static void main(String[] args) {
+        Integer a = 500;
+//        Principle of autoboxing converts the object directly to a primitive type.
+        int k = a;
+
+        System.out.println(a);
+        System.out.println(k);
+    }
+}
